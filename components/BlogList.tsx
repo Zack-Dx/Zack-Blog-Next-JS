d46acb2ee667a9285da3f1e2a10ffe33cff07c23
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-03-18 00:03:11 +0530

    Blog List component 70% done.

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,11 +1,55 @@
 import React from "react";
-
+import urlFor from "../lib/urlFor";
+import Image from "next/image";
 type Props = {
   posts: Post[];
 };
 
 const BlogList = ({ posts }: Props) => {
-  return <div>BlogList</div>;
+  return (
+    <div>
+      <hr className="border-[#F7AB0A] mb-10" />
+      <div>
+        {posts.map((post) => (
+          <div key={post._id} className="flex flex-col group">
+            <div className="relative w-full h-80 drop-shadow-xl  transition-transform duration-200 ease-out">
+              <Image
+                className="object-cover object-left lg:object-center"
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+              <div>
+                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex items-center justify-between ">
+                  <div>
+                    <p className="font-bold">{post.title}</p>
+                    <p>{new Date(post._createdAt).toLocaleDateString()}</p>
+                  </div>
+                  <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2">
+                    {post.categories &&
+                      post.categories.map((category) => {
+                        return (
+                          <div
+                            key={category._id}
+                            className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                          >
+                            <p>{category.title.toString()}</p>
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold">{post.title}</p>
+              <p className="text-gray-500">{post.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogList;
--- typings.d.ts
+++ typings.d.ts
@@ -34,7 +34,62 @@ interface Reference {
 }
 
 interface Slug {
-  _type: Slug;
+  _type: "slug";
+  current: string;
+}
+
+interface Block {
+  _key: string;
+  _type: "block";
+  children: Span[];
+  markDefs: any[];
+  style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+}
+
+interface Span {
+  _key: string;
+  _type: "span";
+  marks: string[];
+  text: string;
+}
+
+type Base = {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+};
+
+interface Post extends Base {
+  author: Author;
+  body: Block[];
+  categories: Category[];
+  mainImage: Image;
+  slug: Slug;
+  title: string;
+  description: string;
+}
+
+interface Author extends Base {
+  bio: Block[];
+  image: Image;
+  name: string;
+  slug: Slug;
+}
+
+interface Image {
+  _type: "image";
+  asset: Reference;
+}
+
+interface Reference {
+  _ref: string;
+  _type: "reference";
+}
+
+interface Slug {
+  _type: string;
   current: string;
 }
 
@@ -54,8 +109,8 @@ interface Span {
 }
 
 interface Category extends Base {
-  description: true;
-  title: String;
+  description: string;
+  title: string;
 }
 
 interface MainImage {
