fbfb7462bee199f73727b87d8bfd49a0cd3b5d39
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-03-18 15:31:39 +0530

    80% ui completed.

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import urlFor from "../lib/urlFor";
 import Image from "next/image";
+import ClientSideRoute from "./ClientSideRoute";
 type Props = {
   posts: Post[];
 };
@@ -9,43 +11,49 @@ const BlogList = ({ posts }: Props) => {
   return (
     <div>
       <hr className="border-[#F7AB0A] mb-10" />
-      <div>
-        {posts.map((post) => (
-          <div key={post._id} className="flex flex-col group">
-            <div className="relative w-full h-80 drop-shadow-xl  transition-transform duration-200 ease-out">
-              <Image
-                className="object-cover object-left lg:object-center"
-                src={urlFor(post.mainImage).url()}
-                alt={post.author.name}
-                fill
-              />
-              <div>
-                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex items-center justify-between ">
-                  <div>
-                    <p className="font-bold">{post.title}</p>
-                    <p>{new Date(post._createdAt).toLocaleDateString()}</p>
-                  </div>
-                  <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2">
-                    {post.categories &&
-                      post.categories.map((category) => {
-                        return (
-                          <div
-                            key={category._id}
-                            className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
-                          >
-                            <p>{category.title.toString()}</p>
-                          </div>
-                        );
-                      })}
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
+        {posts.map((post, index) => (
+          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+            <div className="flex flex-col group">
+              <div className="relative w-full h-80 drop-shadow-xl transform hover:scale-105 transition duration-200 ease-out">
+                <Image
+                  className="object-cover lg:object-center"
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  fill
+                />
+                <div>
+                  <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex items-center justify-between ">
+                    <div>
+                      <p className="font-bold">{post.title}</p>
+                      <p>{new Date(post._createdAt).toLocaleDateString()}</p>
+                    </div>
+                    <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2">
+                      {post.categories &&
+                        post.categories.map((category) => {
+                          return (
+                            <div
+                              key={category._id}
+                              className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                            >
+                              <p>{category.title.toString()}</p>
+                            </div>
+                          );
+                        })}
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className="mt-5 flex-1">
+                <p className="underline text-lg font-bold">{post.title}</p>
+                <p className="line-clamp-2 text-gray-500">{post.description}</p>
+              </div>
+              <p className="mt-5 font-bold flex items-center group-hover:underline">
+                Read Post
+                <ArrowRightIcon className="ml-2 h-4 w-4" />
+              </p>
             </div>
-            <div className="mt-5 flex-1">
-              <p className="underline text-lg font-bold">{post.title}</p>
-              <p className="text-gray-500">{post.description}</p>
-            </div>
-          </div>
+          </ClientSideRoute>
         ))}
       </div>
     </div>
---
+++ components/ClientSideRoute.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+const ClientSideRoute = ({
+  children,
+  route,
+}: {
+  children: React.ReactNode;
+  route: string;
+}) => {
+  return <Link href={route}>{children}</Link>;
+};
+
+export default ClientSideRoute;
